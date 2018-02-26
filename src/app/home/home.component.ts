d59d81580733cb52014e3bb269149d9c6a25a530
Travis <tostahow@gmail.com>
2018-02-26 00:02:57 -0700

    Add initial project files

---
+++ src/app/home/home.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent implements OnInit {
+
+  itemCount: number;
+  btnText: string = 'Add an item';
+  goalText: string = 'My First Life Goal';
+  goals = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    this.itemCount = this.goals.length;
+
+  }
+
+  addItem() {
+    this.goals.push(this.goalText);
+    this.goalText = '';
+    this.itemCount =  this.goals.length;
+  }
+
+}
